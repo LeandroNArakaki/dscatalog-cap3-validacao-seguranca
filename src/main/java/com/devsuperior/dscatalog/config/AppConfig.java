71aebfc8a16750a3492d2c452c2738e3c78040eb
Leandro Arakaki <leanarakaki@gmail.com>
2024-02-19 20:48:20 -0400

    Criado a classe SecurityConfig

--- src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -2,9 +2,7 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
@@ -14,14 +12,5 @@ public class AppConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
-	@Configuration
-	public class SecurityConfig {
 
-		@Bean
-		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-			http.csrf(csrf -> csrf.disable());
-			http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-			return http.build();
-		}
-	}
 }
