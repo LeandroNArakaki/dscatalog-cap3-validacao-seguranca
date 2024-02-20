b934169cecdae91cea0473e6579b208665851d78
Leandro Arakaki <leanarakaki@gmail.com>
2024-02-19 20:35:37 -0400

    Criado metodos na classe User

--- src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -90,6 +90,21 @@ public class User {
 	public Set<Role> getRoles() {
 		return roles;
 	}
+
+	public void addRole(Role role){
+		roles.add(role);
+	}
+
+	public boolean hasRole(String roleName){
+		for (Role role : roles){
+			if(role.getAuthority().equals(roleName)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
 	
 	@Override
 	public int hashCode() {
