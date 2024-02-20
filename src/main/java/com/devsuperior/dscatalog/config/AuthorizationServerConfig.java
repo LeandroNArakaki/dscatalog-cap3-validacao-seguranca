51294e4ab0cb719efca2f10cd2104c1fd472173c
Leandro Arakaki <leanarakaki@gmail.com>
2024-02-20 10:21:35 -0400

    Correção de imports

--- src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dscatalog.config;
 
 
+import com.devsuperior.dscatalog.config.customgrant.CustomPasswordAuthenticationConverter;
+import com.devsuperior.dscatalog.config.customgrant.CustomPasswordAuthenticationProvider;
+import com.devsuperior.dscatalog.config.customgrant.CustomUserAuthorities;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
